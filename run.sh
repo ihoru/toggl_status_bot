099666806f1777ad4ef41628863dcd35b7da023f
Igor Polyakov <ihor.polyakov@gmail.com>
2023-07-01 18:41:58 +0700

    Deployment

--- run.sh
+++ run.sh
@@ -3,5 +3,7 @@
 DIR=$(dirname $0)
 cd "${DIR}" || exit
 LOG="${DIR}/logs/run.log"
+# shellcheck disable=SC2155
+export PATH="$(cat .node_path):$PATH"
 export NODE_ENV="production"
-npm run start:prod >"$LOG" 2>&1
+node "${DIR}/src/index.js" >"$LOG" 2>&1
