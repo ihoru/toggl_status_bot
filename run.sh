fa7289c6fa3861e055438a5feeb50b12ba4c9a6a
Igor Polyakov <ihor.polyakov@gmail.com>
2023-07-01 18:42:51 +0700

    Deployment setup

--- run.sh
+++ run.sh
@@ -6,4 +6,5 @@ LOG="${DIR}/logs/run.log"
 # shellcheck disable=SC2155
 export PATH="$(cat .node_path):$PATH"
 export NODE_ENV="production"
-node "${DIR}/src/index.js" >"$LOG" 2>&1
+date 2>"$LOG" 2>&1
+node "${DIR}/src/index.js" 2>"$LOG" 2>&1
