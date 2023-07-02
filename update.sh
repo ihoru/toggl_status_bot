34f4d33cfb07e96cb78f636dc665af66fd5cd48a
Igor Polyakov <ihor.polyakov@gmail.com>
2023-07-02 17:19:40 +0700

    type in supervisorctl

--- update.sh
+++ update.sh
@@ -6,4 +6,4 @@ cd "${DIR}" || exit
 git checkout . &&
 git pull &&
 npm install &&
-supervisorctrl restart toggl_status_bot && echo "SUCCESS" || echo "FAIL"
+supervisorctl restart toggl_status_bot && echo "SUCCESS" || echo "FAIL"
